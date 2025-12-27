841212d33bb62ad9856970aeda22f12f0545724f
dayane <dayalvesdev89@gmail.com>
2025-12-27 12:05:07 -0300

    ADCIONEI MOEDAS

--- scripts.js
+++ scripts.js
@@ -1,36 +1,49 @@
 const convertButton = document.querySelector(".convert-button");
 const currencySelect = document.querySelector(".currency-select");
 
-function convertValues() {
+ async function convertValues() {
    const inputCurrencyValue = document.querySelector(".input-currency").value
    const currencyValueToConvert = document.querySelector(".currency-value-to-convert")
    const currencyValueConverted = document.querySelector(".currency-value")
 
+   const data = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").then(response => response.json());
 
-   const dolarToday = 5.2
-   const euroToday = 6.2
+   const dolarToday = data.USDBRL.high;
+   const euroToday = data.EURBRL.high;
+   const btcToday = data.BTCBRL.high;
+
+   
+    
+
+   
 
 
-
-   if (currencySelect.value === "dolar") {
+   if (currencySelect.value == "dolar") {
       //se o valor do select for dolar, faça a conversão para dolar
-
-
       currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
          style: "currency",
          currency: "USD",
       }).format(inputCurrencyValue / dolarToday)
 
-
    }
 
-   if (currencySelect.value === "euro") {
+   if (currencySelect.value == "euro") {
       //se o valor do select for euro, faça a conversão para euro
       currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
          style: "currency",
          currency: "EUR",
       }).format(inputCurrencyValue / euroToday)
    }
+
+  if (currencySelect.value == "cripto") {
+        // Bitcoin geralmente usa muitas casas decimais ou formato específico
+        const convertedBtc = inputCurrencyValue / (btcToday * 1000); 
+        // API retorna valor em milhar às vezes, ajuste conforme necessário
+        currencyValueConverted.innerHTML = "₿ " + (inputCurrencyValue / btcToday).toFixed(6);
+    }
+
+
+
 
 
 
@@ -58,7 +71,14 @@ function changeCurrency() {
    }
 
 
-   convertValues()
+if (currencySelect.value == "cripto") {
+        currencyName.innerHTML = "Bitcoin";
+        currencyImage.src = "./assets/img/bitcoin.png";
+         // Certifique-se de ter essa imagem
+    }
+
+convertValues();
+
 }
 
 currencySelect.addEventListener("change", changeCurrency)
